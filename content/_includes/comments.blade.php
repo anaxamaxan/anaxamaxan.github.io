f34f7c8cfe025d8512cfe560080e9c0653819a8a
Max Schwanekamp <max@celearningsystems.com>
2016-05-16 02:09:52 -0700

    remove some code comments, fix disqus identifier

--- content/_includes/comments.blade.php
+++ content/_includes/comments.blade.php
@@ -1,20 +1,12 @@
 <div id="disqus_thread"></div>
 <script>
-/**
-* RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
-* LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables
-*/
-
 var disqus_config = function () {
 this.page.url = '{{$siteBaseUrl}}@url($currentUrlPath)';
-this.page.identifier = '{{ str_replace('/','-',ltrim($currentUrlPath,'/')) }}';
+this.page.identifier = '{{ str_replace('/','-',trim($currentUrlPath,'/')) }}';
 };
-
 (function() { // DON'T EDIT BELOW THIS LINE
 var d = document, s = d.createElement('script');
-
 s.src = '//anaxamaxan.disqus.com/embed.js';
-
 s.setAttribute('data-timestamp', +new Date());
 (d.head || d.body).appendChild(s);
 })();
